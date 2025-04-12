92e5bb9331c4c4e8e5dc6773d72f81459e7dde85
António Macave <antonio.macave@gmail.com>
2025-04-12 16:41:50 +0200

    Identifiers class added.

---
+++ src/com/ujc/antoniomacave/cryptomaster/util/Identifier.java
@@ -0,0 +1,10 @@
+package com.ujc.antoniomacave.cryptomaster.util;
+
+
+public class Identifier {
+    
+    public final static String CEASER_CIPHER = "Cifra de César";
+    public final static String HILL_CIPHER = "Cifra de Hill";
+    public final static String VIGENERE_CIPHER = "Cifra de Vigenère";
+    
+}
