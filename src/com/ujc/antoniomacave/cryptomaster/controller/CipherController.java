c864e80b818117e3a2481bfd4998da62f2f2b172
António Macave <antonio.macave@gmail.com>
2025-04-12 13:50:18 +0200

    Implementing copy to clipboard

---
+++ src/com/ujc/antoniomacave/cryptomaster/controller/CipherController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ujc.antoniomacave.cryptomaster.controller;
+
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
+/**
+ *
+ * @author Macave
+ */
+public class CipherController {
+    
+    private void copyText(String text) {
+        StringSelection stringSelection = new StringSelection(text);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+    }
+    
+}
