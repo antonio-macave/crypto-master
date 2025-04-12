905324df96b7be3045bb8d826768a8ebf8738076
António Macave <antonio.macave@gmail.com>
2025-04-12 16:35:36 +0200

    Changes made on UI

--- src/com/ujc/antoniomacave/cryptomaster/view/Ciphers.java
+++ src/com/ujc/antoniomacave/cryptomaster/view/Ciphers.java
@@ -5,6 +5,7 @@
  */
 package com.ujc.antoniomacave.cryptomaster.view;
 
+import com.ujc.antoniomacave.cryptomaster.controller.CipherController;
 import javax.swing.JFrame;
 
 /**
@@ -24,7 +25,6 @@ public class Ciphers extends javax.swing.JFrame {
     private void startApp() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Cifras");
-        setSize(600,500);
         setLocationRelativeTo(null);
         setVisible(true);
     }
@@ -39,76 +39,126 @@ public class Ciphers extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        btnHillCipher = new javax.swing.JButton();
-        btnCeaserCipher = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
         btnVigenereCipher = new javax.swing.JButton();
+        btnCeaserCipher = new javax.swing.JButton();
+        btnHillCipher = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Cifras");
         setResizable(false);
         setSize(new java.awt.Dimension(500, 500));
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                formWindowClosing(evt);
+            }
+        });
 
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Cifrar mensagens é uma das técnicas para que haja uma comunicação segura.");
+        jLabel1.setText("<html><div style='text-align: center; vertical-align: middle;'>A criptografia de mensagens é uma das estratégias mais seguras para preservar a <b>confidencialidade</b> na troca de dados.</html>");
 
-        btnHillCipher.setLabel("Hill");
-        btnHillCipher.setMargin(new java.awt.Insets(2, 30, 2, 30));
+        jPanel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(153, 153, 153), 1, true));
+
+        btnVigenereCipher.setLabel("Vigenère");
 
         btnCeaserCipher.setText("César");
+        btnCeaserCipher.setRequestFocusEnabled(false);
         btnCeaserCipher.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnCeaserCipherActionPerformed(evt);
             }
         });
 
-        btnVigenereCipher.setLabel("Vigenère");
+        btnHillCipher.setLabel("Hill");
+        btnHillCipher.setMargin(new java.awt.Insets(2, 30, 2, 30));
 
         jLabel2.setText("Escolha uma das cifras abaixo:");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(169, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addGap(166, 166, 166))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(60, 60, 60)
+                .addComponent(btnCeaserCipher)
+                .addGap(82, 82, 82)
+                .addComponent(btnHillCipher)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel1Layout.createSequentialGroup()
+                    .addGap(337, 337, 337)
+                    .addComponent(btnVigenereCipher)
+                    .addContainerGap(70, Short.MAX_VALUE)))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(jLabel2)
+                .addGap(32, 32, 32)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnHillCipher)
+                    .addComponent(btnCeaserCipher))
+                .addContainerGap(31, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel1Layout.createSequentialGroup()
+                    .addGap(76, 76, 76)
+                    .addComponent(btnVigenereCipher)
+                    .addContainerGap(30, Short.MAX_VALUE)))
+        );
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 10)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(102, 102, 102));
+        jLabel3.setText("António Macave, UJC- 3º ano (Pós-Laboral)");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(btnCeaserCipher)
-                .addGap(73, 73, 73)
-                .addComponent(btnHillCipher)
-                .addGap(79, 79, 79)
-                .addComponent(btnVigenereCipher)
-                .addGap(91, 91, 91))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 541, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(205, 205, 205)
-                        .addComponent(jLabel2)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(37, 37, 37)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(21, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabel3)
+                .addGap(20, 20, 20))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(33, 33, 33)
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 75, Short.MAX_VALUE)
-                .addComponent(jLabel2)
-                .addGap(33, 33, 33)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnHillCipher)
-                    .addComponent(btnCeaserCipher)
-                    .addComponent(btnVigenereCipher))
-                .addGap(108, 108, 108))
+                .addGap(38, 38, 38)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 19, Short.MAX_VALUE)
+                .addComponent(jLabel3)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnCeaserCipherActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCeaserCipherActionPerformed
-        // TODO add your handling code here:
+        CifraCesar cifraCesar = new CifraCesar("Ceaser");
+        CipherController cipherController = new CipherController(cifraCesar);
+        dispose();
     }//GEN-LAST:event_btnCeaserCipherActionPerformed
+
+    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
+        // TODO add your handling code here:
+    }//GEN-LAST:event_formWindowClosing
 
     /**
      * @param args the command line arguments
@@ -149,5 +199,7 @@ public class Ciphers extends javax.swing.JFrame {
     private javax.swing.JButton btnVigenereCipher;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
