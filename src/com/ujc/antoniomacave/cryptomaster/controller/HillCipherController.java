8c50e439c269fa735278aa08087a562efc72eeaf
António Macave <antonio.macave@gmail.com>
2025-04-23 20:28:24 -0700

    Hill Controller created

---
+++ src/com/ujc/antoniomacave/cryptomaster/controller/HillCipherController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ujc.antoniomacave.cryptomaster.controller;
+
+import com.ujc.antoniomacave.cryptomaster.view.HillForm;
+
+/**
+ *
+ * @author Macave
+ */
+public class HillCipherController {
+    
+    private HillForm hillFormView;
+    
+    public HillCipherController(HillForm hillFormView) {
+        this.hillFormView = hillFormView;
+    }
+    
+}
