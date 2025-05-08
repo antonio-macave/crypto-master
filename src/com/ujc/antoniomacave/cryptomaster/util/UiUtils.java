b05a9cbb9830be72b9c2be0b33e67601d6459393
António Macave <antonio.macave@gmail.com>
2025-05-08 11:03:36 +0200

    Renaming the method

--- src/com/ujc/antoniomacave/cryptomaster/util/UiUtils.java
+++ src/com/ujc/antoniomacave/cryptomaster/util/UiUtils.java
@@ -9,8 +9,10 @@ import javax.swing.JTextArea;
  * @author Macave
  */
 public class UiUtils {
-    
-    public static void setMode(boolean isEncryptMode, JButton button, JLabel inputLabel, JLabel outputLabel, JTextArea outputField) {
+    /*
+    Modifies labels according to the selected RadioButton
+    */
+    public static void setActionMode(boolean isEncryptMode, JButton button, JLabel inputLabel, JLabel outputLabel, JTextArea outputField) {
         if (isEncryptMode) {
             button.setText("Encriptar");
             inputLabel.setText("Texto plano");
