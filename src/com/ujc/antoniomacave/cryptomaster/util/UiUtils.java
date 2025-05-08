032a490a9467116738e92d5aada69c96c8c3aca3
António Macave <antonio.macave@gmail.com>
2025-05-08 10:57:57 +0200

    Adding UiUtils

---
+++ src/com/ujc/antoniomacave/cryptomaster/util/UiUtils.java
@@ -0,0 +1,27 @@
+package com.ujc.antoniomacave.cryptomaster.util;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author Macave
+ */
+public class UiUtils {
+    
+    public static void setMode(boolean isEncryptMode, JButton button, JLabel inputLabel, JLabel outputLabel, JTextArea outputField) {
+        if (isEncryptMode) {
+            button.setText("Encriptar");
+            inputLabel.setText("Texto plano");
+            outputLabel.setText("Texto cifrado");
+            outputField.setText("");
+        } else {
+            button.setText("Decriptar");
+            inputLabel.setText("Texto cifrado");
+            outputLabel.setText("Texto plano");
+            outputField.setText("");
+        }
+    }
+    
+}
